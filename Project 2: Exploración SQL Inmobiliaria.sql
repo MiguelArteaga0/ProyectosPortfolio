d9807425f67ebe6b95b1c8e03e6647f6081345c9
MiguelArteaga0 <114202657+MiguelArteaga0@users.noreply.github.com>
2022-11-05 13:59:03 -0600

    Update Project 2: Exploración SQL Inmobiliaria.sql

--- Project 2: Exploración SQL Inmobiliaria.sql
+++ Project 2: Exploración SQL Inmobiliaria.sql
@@ -1,9 +1,27 @@
+-- Conseguir dato de Casas 2022
+
 SELECT
 Municipio,
 Inmueble, 
-AVG(Precio) as Avg_Precio,
+ROUND(AVG(Precio),2) as Avg_Precio_MXN,
 COUNT(Inmueble) as Count_Inmueble
 FROM
 Portfolio2..Venta2022
+WHERE Inmueble = 'Casa'
+Group by Municipio, Inmueble
+Order by Municipio ASC
+
+
+
+-- Conseguir dato de Departamentos 2022
+
+SELECT
+Municipio,
+Inmueble, 
+ROUND(AVG(Precio),2) as Avg_Precio_MXN,
+COUNT(Inmueble) as Count_Inmueble
+FROM
+Portfolio2..Venta2022
+WHERE Inmueble = 'Departamento'
 Group by Municipio, Inmueble
 Order by Municipio ASC
